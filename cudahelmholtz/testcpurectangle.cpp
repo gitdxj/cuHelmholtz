bcf4057c8476d1673d70e11da61163bb3768c191
rmingming <renmingming@nbjl.nankai.edu.cn>
2019-09-25 23:22:52 +0800

    Add files via upload

---
+++ cudahelmholtz/testcpurectangle.cpp
@@ -0,0 +1,181 @@
+#include "hw3crt_wrapper.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include "time_.h"
+
+#define PI 3.14159265358979323846
+//#define DEBUG
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if (argc < 5)
+	{
+		printf("usage: ./t Nx Ny Nz xbc ybc zbc\n");
+		exit(-1);
+	}
+	int Nx = atoi(argv[1]);
+	int Ny = atoi(argv[2]);
+	int Nz = atoi(argv[3]);
+	int xbc = atoi(argv[4]);
+	int ybc = atoi(argv[5]);
+	int zbc = atoi(argv[6]);
+	int nx=Nx; 
+	int ny=Ny;
+	int nz=Nz;
+	int i, j, k;
+	double dx = 1.0/nx;
+	double dy = 1.0/ny;
+	double dz = 1.0/nz;
+	int sz = (nx+1)*(ny+1)*(nz+1);
+	double *f =  new double[sz];
+	double *bcl=NULL;
+	double *bcr=NULL;
+	double *bcb=NULL;
+	double *bct=NULL;
+	double *bcf=NULL;
+	double *bce=NULL;
+
+	if ( xbc == 3 || xbc == 4 )
+		bcl = (double *) malloc(sizeof(double)*(ny+1)*(nz+1));
+	if ( xbc == 3 || xbc == 2 )
+		bcr = (double *) malloc(sizeof(double)*(ny+1)*(nz+1));
+	if ( ybc == 3 || ybc == 4 )
+		bcb = (double *) malloc(sizeof(double)*(nx+1)*(nz+1));
+	if ( ybc == 3 || ybc == 2 )
+		bct = (double *) malloc(sizeof(double)*(nx+1)*(nz+1));
+	if ( zbc == 3 || zbc == 4 )
+		bcf = (double *) malloc(sizeof(double)*(nx+1)*(ny+1));
+	if ( zbc == 3 || zbc == 2 )
+		bce = (double *) malloc(sizeof(double)*(nx+1)*(ny+1));
+
+	//D
+	for (k=0; k<nz+1; k++)
+		for (j=0; j<ny+1; j++)
+			for (i=0; i<nx+1; i++)
+				f[k*(nx+1)*(ny+1)+j*(nx+1)+i] = 
+					(-1./3-4*PI*PI)*sin(2*PI*i*dx)*sin(2*PI*j*dy)*sin(2*PI*k*dz);
+	//				(-1./3-4*PI*PI)*cos(2*PI*i*dx)*cos(2*PI*j*dy)*cos(2*PI*k*dz);
+					// - (i*dx)*(i*dx) - (j*dy)*(j*dy);
+	//f[100]+=0.1;
+	for (k=0; k<nz+1; k++)
+		for (j=0; j<ny+1; j++)
+		{
+			double temp = 2. * PI / 3. * sin(2*PI*j*dy) * sin(2*PI*k*dz);
+			//double temp = 0.;
+			if ( xbc == 3 || xbc == 4 )
+				bcl[k*(ny+1)+j] = temp;// k*dz*k*dz + j*dy*j*dy +0;
+			if ( xbc == 3 || xbc == 2 )
+				bcr[k*(ny+1)+j] = temp;// k*dz*k*dz + j*dy*j*dy +1;
+		}
+////			f[k*(nx+1)*(ny+1)+j*(nx+1)+0] 
+////				= k*dz*k*dz + j*dy*j*dy +0;
+////			f[k*(nx+1)*(ny+1)+j*(nx+1)+nx]
+////				= k*dz*k*dz + j*dy*j*dy +1;
+//		}
+
+	for (k=0; k<nz+1; k++)
+		for (i=0; i<nx+1; i++)
+		{
+			double temp = 2. * PI / 3. * sin(2*PI*i*dx) * sin(2*PI*k*dz);
+			//double temp = 0.;
+			if ( ybc == 3 || ybc == 4 )
+				bcb[k*(nx+1)+i] = temp;// k*dz*k*dz + j*dy*j*dy +0;
+			if ( ybc == 3 || ybc == 2 )
+				bct[k*(nx+1)+i] = temp;// k*dz*k*dz + j*dy*j*dy +1;
+	//		f[k*(nx+1)*(ny+1)+0*(nx+1)+i] = 0;
+	//			= k*dz*k*dz + i*dx*i*dx +0;
+	//		f[k*(nx+1)*(ny+1)+ny*(nx+1)+i] = 0; 
+	//			= k*dz*k*dz + i*dx*i*dx +1;
+		}
+
+	for (j=0; j<ny+1; j++)
+		for (i=0; i<nx+1; i++)
+		{
+			double temp = 2. * PI / 3. * sin(2*PI*i*dx) * sin(2*PI*j*dy);
+			//double temp = 0.;
+			if ( zbc == 3 || zbc == 4 )
+				bcf[j*(nx+1)+i] = temp;// k*dz*k*dz + j*dy*j*dy +0;
+			if ( zbc == 3 || zbc == 2 )
+				bce[j*(nx+1)+i] = temp;// k*dz*k*dz + j*dy*j*dy +1;
+		}
+
+#ifdef DEBUG1
+	for (k=0; k<=nz; k++)
+	{
+		for (j=0; j<=ny; j++)
+		{
+			for (i=0; i<=nx; i++)
+			{
+				printf("%.14f\t", f[k*(nx+1)*(ny+1)+j*(nx+1)+i]);
+				//printf("%.14f\t", (f[i+1]-f[i-1])/2./dx);
+				//if (i%5 == 4)
+				//	printf("\n");
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+#endif
+
+	time_(
+	solver_3d(0, 1, nx, xbc, bcl, bcr, 
+			  0, 1, ny, ybc, bcb, bct,  
+			  0, 1, nz, zbc, bcf, bce, 
+			  -1, f);
+	)
+
+	printf("***********************************\n");
+
+	double maxim = 0;
+	for (k=0; k<=nz; k++)
+	{
+		for (j=0; j<=ny; j++)
+		{
+			for (i=0; i<=nx; i++)
+			{
+				//double dif = f[k*(nx+1)*(ny+1)+j*(nx+1)+i] - 1./3*cos(2*PI*i*dx) *cos(2*PI*j*dy)*cos(2*PI*k*dz);
+				double dif = f[k*(nx+1)*(ny+1)+j*(nx+1)+i] - 1./3*sin(2*PI*i*dx) *sin(2*PI*j*dy)*sin(2*PI*k*dz);
+				//- (k*dz*k*dz + j*dy*j*dy  + i*dx*i*dx);
+#ifdef DEBUG
+				printf("%12.8f", dif);
+#endif
+				dif = fabs(dif);
+				if (maxim < dif)
+					maxim = dif;
+//				printf("%.14f\t", f[k*(nx+1)*(ny+1)+j*(nx+1)+i]);// -sin(2*PI*i*dx)*sin(2*PI*j*dy)*sin(2*PI*k*dz));
+						//- (k*dz*k*dz + j*dy*j*dy  + i*dx*i*dx));
+				//printf("%.14f\t", (f[i+1]-f[i-1])/2./dx);
+#ifdef DEBUG
+				if (i%9 == 8)
+					printf("\n");
+#endif
+			}
+			//printf("\n");
+		}
+#ifdef DEBUG
+		printf("\n");
+#endif
+	}
+
+	printf("max diff : %.14f\n", maxim);
+
+	delete [] f;
+	if ( xbc == 3 || xbc == 4 )
+		free(bcl);
+	if ( xbc == 3 || xbc == 2 )
+		free(bcr);
+	if ( ybc == 3 || ybc == 4 )
+		free(bcb);
+	if ( ybc == 3 || ybc == 2 )
+		free(bct);
+	if ( zbc == 3 || zbc == 4 )
+		free(bcf);
+	if ( zbc == 3 || zbc == 2 )
+		free(bce);
+
+	return 0;
+}
